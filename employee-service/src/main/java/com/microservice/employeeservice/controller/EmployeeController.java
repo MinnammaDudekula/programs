331699cb6e51a3cd1b4a54860eb77d8fa09774ca
MinnammaDudekula <97230618+MinnammaDudekula@users.noreply.github.com>
2023-11-28 22:41:45 +0530

    Add files via upload

---
+++ employee-service/src/main/java/com/microservice/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.microservice.employeeservice.controller;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.microservice.employeeservice.dto.APIResponseDto;
+import com.microservice.employeeservice.dto.EmployeeDto;
+import com.microservice.employeeservice.service.EmployeeService;
+
+@RestController
+@RequestMapping("api/employees")
+@AllArgsConstructor
+public class EmployeeController {
+  @Autowired
+    private EmployeeService employeeService;
+
+    // Build Save Employee REST API
+    @PostMapping
+    public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    // Build Get Employee REST API
+    @GetMapping("{id}")
+    public ResponseEntity<APIResponseDto> getEmployee(@PathVariable("id") Long employeeId){
+        APIResponseDto apiResponseDto = employeeService.getEmployeeById(employeeId);
+        return new ResponseEntity<>(apiResponseDto, HttpStatus.OK);
+    }
+}
---
+++ employee-service/src/main/java/com/microservice/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,105 @@
+package com.microservice.employeeservice.service.impl;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.AllArgsConstructor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.microservice.employeeservice.dto.APIResponseDto;
+import com.microservice.employeeservice.dto.DepartmentDto;
+import com.microservice.employeeservice.dto.EmployeeDto;
+import com.microservice.employeeservice.dto.OrganizationDto;
+import com.microservice.employeeservice.entity.Employee;
+import com.microservice.employeeservice.mapper.EmployeeMapper;
+import com.microservice.employeeservice.repository.EmployeeRepository;
+import com.microservice.employeeservice.service.APIClient;
+import com.microservice.employeeservice.service.EmployeeService;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+  @Autowired
+    private EmployeeRepository employeeRepository;
+
+   // private RestTemplate restTemplate;
+  @Autowired
+    private WebClient webClient;
+  @Autowired
+    private APIClient apiClient;
+
+    @Override
+    public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+
+        Employee saveDEmployee = employeeRepository.save(employee);
+
+        EmployeeDto savedEmployeeDto = EmployeeMapper.mapToEmployeeDto(saveDEmployee);
+
+        return savedEmployeeDto;
+    }
+
+    //@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Override
+    public APIResponseDto getEmployeeById(Long employeeId) {
+
+        LOGGER.info("inside getEmployeeById() method");
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://DEPARTMENT-SERVICE/api/departments/" + employee.getDepartmentCode(),
+//                DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = responseEntity.getBody();
+
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
+
+      //  DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+
+        OrganizationDto organizationDto = webClient.get()
+                .uri("http://localhost:8083/api/organizations/" + employee.getOrganizationCode())
+                .retrieve()
+                .bodyToMono(OrganizationDto.class)
+                .block();
+
+        EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDepartment(departmentDto);
+        apiResponseDto.setOrganization(organizationDto);
+        return apiResponseDto;
+    }
+
+    public APIResponseDto getDefaultDepartment(Long employeeId, Exception exception) {
+
+        LOGGER.info("inside getDefaultDepartment() method");
+
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("R&D Department");
+        departmentDto.setDepartmentCode("RD001");
+        departmentDto.setDepartmentDescription("Research and Development Department");
+
+        EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDepartment(departmentDto);
+        return apiResponseDto;
+    }
+}
